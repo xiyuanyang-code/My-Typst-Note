11b973ee4de2a9aa9718aeff6d012018eb162fb5
xiyuanyang-code <xiyuan__yang@outlook.com>
2025-10-26 19:00:32 +0800

    AutoCommit: Release Updates for v1.0.13

--- scripts/update_notes/compile.py
+++ scripts/update_notes/compile.py
@@ -98,7 +98,7 @@ def compile(input_files):
 
     for input_file in tqdm(input_files, total=length, desc="Compiling"):
         output_path = generate_output_path(input_file)
-        root_dir = "/home/xiyuanyang/Note/TypstNote/"
+        root_dir = "/Users/xiyuanyang/Desktop/Dev/Note/TypstNote"
         command_list = [
             "make",
             f"TYPST_ROOT={root_dir}",
